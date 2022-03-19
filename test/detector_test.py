d2b8f40fd922ebd804bce87483a98a3555e183ff
fkite-cs <ortizmiguel.1996@gmail.com>
2022-03-19 12:31:20 +0100

    yolo

---
+++ test/detector_test.py
@@ -0,0 +1,31 @@
+import unittest
+import sys
+import os
+import cv2
+import numpy as np
+import yolo_utils as yu
+
+sys.path.insert(0, os.path.abspath(".."))
+
+class TestVehicleDetector(unittest.TestCase):
+
+    def test_load_img(self):
+
+        opt = {
+            "conf_thres": 0.25,
+            "iou_thres": 0.45,
+            "classes": None,
+            "agnostic_nms": False,
+            "max_det": 1000
+        }
+
+        weights_path = "best.pt"
+        img_path = "imgs/austin1_cropped_2.jpg"
+        img = cv2.imread(img_path)
+        img = img.astype(np.float32)
+        model, imgsz, stride, pt, names = yu.load_model(weights=weights_path)
+        preds = yu.detect2(model, "imgs/", imgsz, pt, stride, opt)
+        self.assertGreater(len(preds), 0)
+
+if __name__ == "__main__":
+    unittest.main()
