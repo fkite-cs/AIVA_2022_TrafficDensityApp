49dba484fe6a1e48375ad3a6b5652148214bde79
fkite-cs <ortizmiguel.1996@gmail.com>
2022-03-19 12:04:10 +0100

    test

--- test/dummy_test.py
+++ test/dummy_test.py
@@ -5,7 +5,7 @@ class TestDummy(unittest.TestCase):
 
     def test_dummy_1(self):
         a = 2 + 2
-        self.assertEquals(a, 4)
+        self.assertEquals(a, 5)
 
 if __name__ == "__main__":
     unittest.main()
