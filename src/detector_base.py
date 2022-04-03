5a875a008ae712f28bd13bd38ce5db88938eb4a7
fkite-cs <ortizmiguel.1996@gmail.com>
2022-04-03 18:27:24 +0200

    #5 detector base test

--- src/detector_base.py
+++ src/detector_base.py
@@ -1,3 +1,6 @@
+import numpy as np
+import src.yolo_utils as yu
+
 class DetectorBase():
 
     def __init__(self, model_type, config_model) -> None:
@@ -12,8 +15,7 @@ class DetectorBase():
             self.model = SSD(config_model)
 
     def forward(self, img):
-        pass
-
+        return self.model.forward(img)
 
 
 class ModelInterface():
@@ -30,12 +32,19 @@ class ModelInterface():
 
 class YOLO(ModelInterface):
     
-    def __init__(self) -> None:
+    def __init__(self, config) -> None:
         super().__init__()
+        self.config = config
+        self.__load_model()
 
+    def __load_model(self):
+        self.model = yu.load_model(self.config.weights_path, self.config.device)
+
+    def forward(self, img):
+        return np.random.randint(low=0,high=10, size=(10,5,4))
 
 class SSD(ModelInterface):
 
-    def __init__(self) -> None:
+    def __init__(self, config) -> None:
         super().__init__()
-
+        self.config = config
