07b3e1f6145870995ee46b4b0051c3101b261f83
fkite-cs <ortizmiguel.1996@gmail.com>
2022-04-03 17:02:51 +0200

    cropmanager implementation v1

--- src/cropmanager.py
+++ src/cropmanager.py
@@ -1,13 +1,19 @@
+from src.vehicle import Vehicle
+
+
 class CropManager():
 
-    def __init__(self) -> None:
-        print("hola")
+    def __init__(self, crop_size=250) -> None:
+        #TODO añadir crop_size (?)
+        self.crop_list = []
+        self.crop_size = crop_size
 
     def create_crops(self, img):
         """
             crops: (250,250)
             CropImg
         """
+        #TODO
         self.crop_list = None
 
     def add_vehicles(self, bboxs):
@@ -15,19 +21,31 @@ class CropManager():
             bboxs: (m,n,4)
                 m: number of crops
         """
-        for i, c in enumerate(self.crop_list):
-            c.set_vehicle(bboxs[i])
+        for i in range(len(self.crop_list)):
+            self.crop_list[i].set_vehicle(bboxs[i])
 
 
 class CropImg():
 
-    def __init__(self, n, crop, hw) -> None:
+    def __init__(self, n, crop, hw, dh, dw) -> None:
         self.id = n
-        self.crop = crop
         self.hw = hw
+        self.dh = dh
+        self.dw = dw
+        self.vehicles_list = []
 
     def set_vehicles(self, bboxs):
         """
             bboxs: (n, 4)
         """
-        self.vehicles_list = bboxs
+        for i in range(len(bboxs)):
+            bb = bboxs[i]
+            self.vehicles_list.append(
+                Vehicle(self.hw, *bb)
+            )
+
+    def get_crop(self, img):
+        return img[
+            self.hw[0]:self.hw[0] + self.dh,
+            self.hw[1]:self.hw[1] + self.dw
+        ]
--- src/vehicle_detector.py
+++ src/vehicle_detector.py
@@ -9,12 +9,13 @@ class VehicleDetector():
         self.cm = CropManager() # 
         self.detector = DetectorBase(model_type, config)
         self.hm = HeatMap()
-       
-        
+
+
     def run(self, img):
         """
             img: (5000,5000)
         """
+        self.img = img
         self.cm.create_crops(img)
         bbox_list = self.detector.forward(self.cm.crop_list)
         self.cm.add_vehicles(bbox_list)
