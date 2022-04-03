8b39a475a668e0191a5008e9fb644ef7c243bc37
fkite-cs <ortizmiguel.1996@gmail.com>
2022-04-03 15:24:44 +0200

    #5 Vehicle detector test. V1

--- test/vehicle_test.py
+++ test/vehicle_test.py
@@ -13,12 +13,32 @@ except Exception as e:
 
 import src.vehicle as vehicle
 
+global_hw = (1,1)
+y = 2
+x = 3
+dx = 4
+dy = 5
+
 class TestVehicle(unittest.TestCase):
 
-
     def test_init_vehicle(self):
-        v = vehicle.Vehicle((1,1), 2, 2, 3, 3)
-        
+        v = vehicle.Vehicle(global_hw, y, x, dy, dx)
+        self.assertEquals(v.global_hw, global_hw)
+        self.assertEquals(v.y, y)
+        self.assertEquals(v.x, x)
+        self.assertEquals(v.dy, dy)
+        self.assertEquals(v.dx, dx)
+    
+    def test_get_bbox(self):
+        v = vehicle.Vehicle(global_hw, y, x, dy, dx)
+        bbox = v.get_bbox()
+        self.assertEquals(bbox, [y,x,dy,dx])
+
+    def test_get_global_coordinates(self):
+        v = vehicle.Vehicle(global_hw, y, x, dy, dx)
+        gc = v.get_global_coordinates()
+        _gc = (global_hw[0] + y, global_hw[1] + x)
+        self.assertEquals(gc, _gc)
 
 if __name__ == "__main__":
     unittest.main()
